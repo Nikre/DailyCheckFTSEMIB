faf351c343360678f6fecc95d7c217190d016c5a
Nicolo <monarca.nicolo@gmail.com>
2022-06-29 20:31:00 +0200

    [ADD] Aggiunta una tabella di prova

--- web.app/src/app/app.component.ts
+++ web.app/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import symbols from '../../../symbolsToBuyExtented.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'web.app';
+export class AppComponent implements OnInit {
+  symbols: any;
+  
+  ngOnInit(): void {
+    console.log('OnInit')
+  }
+
+  constructor() {
+    debugger
+    this.symbols = symbols
+    console.log(symbols)
+  }
+
 }
