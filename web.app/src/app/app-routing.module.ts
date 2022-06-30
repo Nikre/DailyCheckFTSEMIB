118d1ea764db54fedd504d124acb927e47c268e0
Nicolo <monarca.nicolo@gmail.com>
2022-06-30 23:59:13 +0200

    [FIX] Fix the routing

--- web.app/src/app/app-routing.module.ts
+++ web.app/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { DetailComponent } from './detail/detail.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'detail', component: DetailComponent },
 ];
