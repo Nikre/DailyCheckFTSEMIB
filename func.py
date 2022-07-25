e5259064169e1d199cfb110e8034a63ef8b4a7aa
Nicolo <monarca.nicolo@gmail.com>
2022-07-25 23:43:00 +0200

    [ADD] - Aggiunto il backtesting nella parte di python

--- func.py
+++ func.py
@@ -4,6 +4,10 @@ import matplotlib.pyplot as plt
 import ta
 
 # Indicators
+open_label = 'Open'
+close = 'Close'
+high = 'High'
+low = 'Low'
 ema10 = 'EMA 10'
 ema20 = 'EMA 20'
 ema20high = 'EMA 20 High'
@@ -14,8 +18,10 @@ ema200 = 'EMA 200'
 hbb = 'HBB 2.5 std'
 lbb = 'LBB 2.5 std'
 rsi = 'RSI 2'
+signal = 'Signal'
 
 def add_indicators(df):
+    """ This function add to the df all the indicators, MUST BE NOTED as the reference is used """
     df[ema20high] = ta.trend.EMAIndicator(df['High'], window=20).ema_indicator() 
     df[ema20low] = ta.trend.EMAIndicator(df['Low'], window=20).ema_indicator() 
     df[ema144] = ta.trend.EMAIndicator(df['Close'], window=144).ema_indicator()
@@ -23,6 +29,7 @@ def add_indicators(df):
     df[hbb] = ta.volatility.BollingerBands(df['Close'], window_dev=2.5).bollinger_hband()
     df[lbb] = ta.volatility.BollingerBands(df['Close'], window_dev=2.5).bollinger_lband()
     df[rsi] = ta.momentum.RSIIndicator(df['Close'], window=2).rsi()
+    df[signal] = -1  # Column used in backtesting
 
 def check_entry_rayReno_bb(symbol, data):
     if (data['Close'].values < data[lbb].values) & (data['Close'].values > data[ema200].values): 
@@ -33,6 +40,59 @@ def check_exit_rayReno_bb(symbol, data):
     if (data[rsi].values > 50):
         return symbol
     return None
+
+def backtesting_ioInvesto(data, society, symbol):
+    """" Function that backtest a dataset with the ioInvesto strategy """
+    df = data.copy()
+    df.dropna(axis=0, inplace=True)
+    
+    open_order = False
+
+    for index, row in df.iterrows():
+        if not open_order: # Devo valutare la condizione di apertura di una posizione
+            if row[close] > row[ema144] and row[close] > row[ema20high] and row[close] > row[ema20low]: # Condizione soddisfatta
+                df.loc[index, signal] = 1 # Segnale di entry
+                open_order = True # Setto il flag
+        else:
+            if row[close] < row[ema20low]: # devo uscire
+                df.loc[index, signal] = -1 # Segnale di entry
+                open_order = False # Setto il flag
+            else:
+                df.loc[index, signal] = 1 # Mantengo la posizione
+    
+    df[signal] = df[signal].shift(1) 
+    # the shift is necessary becouse the order is placed the next day, 
+    # and with the same logic must be applied to the order closing.
+    df.dropna(axis=0, inplace=True)
+    # simply, the row can be deleted, no carry on of information.
+
+    # Filling the array
+    open_order = False
+    orders = []
+    temp = {}
+
+    for index, row in df.iterrows():
+        if row['Signal'] == 1:
+            if not open_order: # devo salvarmi i dati
+                temp = {
+                    "Symbol": symbol,
+                    "Societa": society,
+                    "Strategy": 1, # ioInvesto è quella che ha il valore di 1
+                    "OpenOrderDate": index.strftime("%d/%m/%Y"),
+                    "EntryPrice": row[open_label] # che è il prezzo d'acquisto
+                }
+                open_order = True
+        elif row['Signal'] == -1:
+            if open_order: # posso chiudere l'ordine
+                temp['CloseOrderDate'] = index.strftime("%d/%m/%Y")
+                temp['ExitPrice'] = row[open_label] # che è il prezzo di vendita
+                
+                orders.append(temp)
+                temp = {}
+                open_order = False
+    
+    return orders
+
 
 def check_entry_ioInvesto(symbol, data): 
     if ((data['Close'].values > data[ema20high].values) & 
--- main.py
+++ main.py
@@ -10,17 +10,20 @@ import sys
 
 def main():
     try:
-        opts, args = getopt.getopt(sys.argv[1:], 'e')
+        opts, args = getopt.getopt(sys.argv[1:], 'eb')
     except getopt.GetoptError as err:
         # print help information and exit:
         print(err)  # will print something like "option -a not recognized"
         sys.exit(2)
 
     extented = False
+    backtest = False
 
     for o, _ in opts:
         if o == '-e':
             extented = True
+        if o == '-b':
+            backtest = True
 
     # Get all the symbols
     symbols = pd.read_html(
@@ -30,13 +33,14 @@ def main():
 
     symbolsToBuy = []
     symbolsToBuyExtended = []
+    backtesting = [] 
     lastSamples = 30
 
     # Iterate
     for symbol, society in zip(symbols, societies):
 
         print(f'Analizzo {society}')
-        df = yf.download(symbol, period='1y')
+        df = yf.download(symbol, interval='1d', start='2010-01-01')
         if (len(df.index) < 200):  # necessario perchè alcuni titoli possono avere pochi dati
             # TODO: aggiungere il simbolo con tutte le cose a null, e poi il FE si occupa di 
             # evitare il click
@@ -48,10 +52,15 @@ def main():
         df = df.round(3) # La round() non è inplace
 
         if (extented):
-            high = df.tail(lastSamples)['High'].values.tolist()
-            low = df.tail(lastSamples)['Low'].values.tolist()
-            openData = df.tail(lastSamples)['Open'].values.tolist()
-            close = df.tail(lastSamples)['Close'].values.tolist()
+            high = df.tail(lastSamples)[func.high].values.tolist()
+            low = df.tail(lastSamples)[func.low].values.tolist()
+            openData = df.tail(lastSamples)[func.open_label].values.tolist()
+            close = df.tail(lastSamples)[func.close].values.tolist()
+
+            highAll = df[func.high].values.tolist()
+            lowAll = df[func.low].values.tolist()
+            openDataAll = df[func.open_label].values.tolist()
+            closeAll = df[func.close].values.tolist()
 
             data = {
                 'Simbolo': symbol,
@@ -68,19 +77,32 @@ def main():
                 'LowerBB': df.tail(1)[func.lbb].values[0],
                 'xAxis': list(map(lambda x: x.strftime("%d/%m"), df.tail(lastSamples).index.tolist())),
                 'yAxisHigh': high,
+                'yAxisHighAll': highAll,
                 'yAxisLow': low,
+                'yAxisLowAll': lowAll,
                 'yAxisOpen': openData,
+                'yAxisOpenAll': openDataAll,
                 'yAxisClose': close,
+                'yAxisCloseAll': closeAll,
                 'yData': [list(x) for x in zip(close, openData, low, high)],
+                'yDataAll': [list(x) for x in zip(closeAll, openDataAll, lowAll, highAll)],
                 'ema200Series': df.tail(lastSamples)[func.ema200].values.tolist(),
+                'ema200SeriesAll': df[func.ema200].values.tolist(),
                 'hbbSeries': df.tail(lastSamples)[func.hbb].values.tolist(),
+                'hbbSeriesAll': df[func.hbb].values.tolist(),
                 'lbbSeries': df.tail(lastSamples)[func.lbb].values.tolist(),
+                'lbbSeriesAll': df[func.lbb].values.tolist(),
                 'rsi': df.tail(lastSamples)[func.rsi].values.tolist(),
+                'rsiAll': df[func.rsi].values.tolist(),
                 'ema20High': df.tail(lastSamples)[func.ema20high].values.tolist(),
+                'ema20HighAll': df[func.ema20high].values.tolist(),
                 'ema20Low': df.tail(lastSamples)[func.ema20low].values.tolist(),
                 'ema144Series': df.tail(lastSamples)[func.ema144].values.tolist()
             }
             symbolsToBuyExtended.append(data)
+
+        if(backtest):
+            backtesting.append(func.backtesting_ioInvesto(df, society, symbol))
 
         if (entryReyReno_bb is not None):
             data = {
@@ -107,6 +129,9 @@ def main():
         with open('symbolsToBuyExtented.json', 'w') as f:
            json.dump(symbolsToBuyExtended, f)
 
+    if (backtest):
+        with open('backtesting.json', 'w') as f:
+           json.dump(backtesting, f)
 
 if __name__ == "__main__":
     main()
