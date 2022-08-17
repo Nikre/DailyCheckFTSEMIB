ec69284901b8e2a8ada1ae626523f035e4cb92ee
Nicolo <monarca.nicolo@gmail.com>
2022-08-17 23:13:43 +0200

    [ADD] - Aggiunte piccole migliorie e cambiamenti di colori

--- web/src/app/dashboard/dashboard.component.ts
+++ web/src/app/dashboard/dashboard.component.ts
@@ -10,24 +10,43 @@ import { DashboardSymbol } from '../models/dashboard-symbol';
 export class DashboardComponent implements OnInit {
   symbols: DashboardSymbol[] = [];
   searchedSymbol: string = '';
-  isLoading: boolean = true
+  isLoading: boolean = true;
+  mustFilter: boolean = false;
 
   constructor(private symbolService: SymbolService) {}
 
   ngOnInit(): void {
     this.symbolService.getSymbols().subscribe((res) => {
       this.symbols = res;
-      this.isLoading = false
+      this.isLoading = false;
     });
   }
 
   searching() {
-    this.isLoading = true
+    this.isLoading = true;
     this.symbolService.getSymbols().subscribe((res) => {
       this.symbols = res.filter((x) =>
         x.society.toLowerCase().includes(this.searchedSymbol.toLowerCase())
-        );
-        this.isLoading = false
+      );
+      this.isLoading = false;
+    });
+  }
+
+  filtering() {
+    this.isLoading = true;
+    console.log(this.mustFilter);
+    this.mustFilter = !this.mustFilter;
+    if (this.mustFilter) {
+      console.log('Sto filtrando');
+      this.symbolService.getSymbols().subscribe((res) => {
+        this.symbols = res.filter((x) => x.strategies != '');
+        this.isLoading = false;
       });
+    } else {
+      this.symbolService.getSymbols().subscribe((res) => {
+        this.symbols = res;
+        this.isLoading = false;
+      });
+    }
   }
 }
